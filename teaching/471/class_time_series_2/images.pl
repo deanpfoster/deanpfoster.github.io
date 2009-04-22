926b62c78162d8710ee34d3a11b69a9eae979150
Dean Foster (gosset) <dean@foster.net>
2009-04-22 16:13:09 -0400

    udate

---
+++ teaching/471/class_time_series_2/images.pl
@@ -0,0 +1,52 @@
+# LaTeX2HTML 96.1 (Feb 5, 1996)
+# Associate images original text with physical files.
+
+$key = q/{displaymath}rho(k)=left{arraycl1;SPMamp;k=00;SPMamp;k=pm1,pm2cdotsarrayright.{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=361 HEIGHT=48 ALIGN=BOTTOM ALT="displaymath97" SRC="img3.gif"  > '; 
+$key = q/{displaymath}rho(k)=alpha^k{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=289 HEIGHT=22 ALIGN=BOTTOM ALT="displaymath149" SRC="img21.gif"  > '; 
+$key = q/{_inline}$X_1=Z_1${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=67 HEIGHT=27 ALIGN=MIDDLE ALT="tex2html_wrap_inline109" SRC="img8.gif"  > '; 
+$key = q/{displaymath}rho(k)=left{arraycl1;SPMamp;k=0sum_i=1^q-kbeta_ibeta_i+kleftsum_i=0^qbeta_i^2right.;SPMamp;k=1,cdots,q0;SPMamp;k;SPMgt;qarrayright.{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=432 HEIGHT=66 ALIGN=BOTTOM ALT="displaymath129" SRC="img14.gif"  > '; 
+$key = q/{displaymath}X_t=X_t-1+Z_t.{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=315 HEIGHT=16 ALIGN=BOTTOM ALT="displaymath107" SRC="img7.gif"  > '; 
+$key = q/{_inline}$|alpha|;SPMlt;1${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=54 HEIGHT=30 ALIGN=MIDDLE ALT="tex2html_wrap_inline147" SRC="img20.gif"  > '; 
+$key = q/{displaymath}X_t=alpha_1X_t-1+cdots+alpha_pX_t-p+Z_t+beta_1Z_t-1+cdots+beta_qZ_t-q{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=484 HEIGHT=20 ALIGN=BOTTOM ALT="displaymath153" SRC="img23.gif"  > '; 
+$key = q/{_inline}$rho(1)=0${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=68 HEIGHT=30 ALIGN=MIDDLE ALT="tex2html_wrap_inline151" SRC="img22.gif"  > '; 
+$key = q/{displaymath}E(X(t))=muqquadCov(X(t),X(t+tau))=gamma(tau){displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=432 HEIGHT=20 ALIGN=BOTTOM ALT="displaymath93" SRC="img1.gif"  > '; 
+$key = q/{_inline}$beta_0=1${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=51 HEIGHT=29 ALIGN=MIDDLE ALT="tex2html_wrap_inline133" SRC="img15.gif"  > '; 
+$key = q/{displaymath}X_t=alpha_1X_t-1+cdots+alpha_pX_t-p+Z_t.{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=388 HEIGHT=19 ALIGN=BOTTOM ALT="displaymath141" SRC="img17.gif"  > '; 
+$key = q/{_inline}$sigma^2_z${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=17 HEIGHT=33 ALIGN=MIDDLE ALT="tex2html_wrap_inline103" SRC="img5.gif"  > '; 
+$key = q/{_inline}$E(X_t))=tmu${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=100 HEIGHT=30 ALIGN=MIDDLE ALT="tex2html_wrap_inline113" SRC="img10.gif"  > '; 
+$key = q/{displaymath}rho(k)=left{arraycl1;SPMamp;k=0beta_1left(1+beta_1^2)right.;SPMamp;k=pm10;SPMamp;otherwisearrayright.{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=389 HEIGHT=66 ALIGN=BOTTOM ALT="displaymath135" SRC="img16.gif"  > '; 
+$key = q/{_inline}$mu${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=11 HEIGHT=18 ALIGN=MIDDLE ALT="tex2html_wrap_inline101" SRC="img4.gif"  > '; 
+$key = q/{_inline}${X_t}${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=38 HEIGHT=30 ALIGN=MIDDLE ALT="tex2html_wrap_inline105" SRC="img6.gif"  > '; 
+$key = q/{_inline}${Z_t}${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=35 HEIGHT=30 ALIGN=MIDDLE ALT="tex2html_wrap_inline95" SRC="img2.gif"  > '; 
+$key = q/{_inline}$X_t=sum_i=1^tZ_i${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=106 HEIGHT=33 ALIGN=MIDDLE ALT="tex2html_wrap_inline111" SRC="img9.gif"  > '; 
+$key = q/{displaymath}X_t=beta_0Z_t+beta_1Z_t-1+cdots+beta_qZ_t-q{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=390 HEIGHT=20 ALIGN=BOTTOM ALT="displaymath127" SRC="img13.gif"  > '; 
+$key = q/{_inline}$X_t-X_t-1=Z_t${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=126 HEIGHT=25 ALIGN=MIDDLE ALT="tex2html_wrap_inline119" SRC="img12.gif"  > '; 
+$key = q/{_inline}$Var(X_t)=tsigma^2t${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=125 HEIGHT=32 ALIGN=MIDDLE ALT="tex2html_wrap_inline115" SRC="img11.gif"  > '; 
+$key = q/{_inline}$X_t${_inline}/;
+$cached_env_img{$key} = ' <IMG WIDTH=21 HEIGHT=27 ALIGN=MIDDLE ALT="tex2html_wrap_inline143" SRC="img18.gif"  > '; 
+$key = q/{displaymath}X_t=alphaX_t-1+Z_t{displaymath}/;
+$cached_env_img{$key} = ' <IMG WIDTH=319 HEIGHT=16 ALIGN=BOTTOM ALT="displaymath145" SRC="img19.gif"  > '; 
+
+1;
+
