afc1811144ff1e5a969eafbdbfd5d323fce94a18
Dean Foster (gosset) <dean@foster.net>
2009-08-02 08:42:48 -0400

    update

--- index.pl
+++ index.pl
@@ -1,13 +1,73 @@
 #!/usr/bin/perl
 
-# This script (c) 1999 Philip Fibiger - pnf1@cornell.edu
-# it can be freely distributed, as long as this comment
-# remains intact
-# version 1.2
-
 use LWP::Simple;
 use CGI qw/:standard/;
 use strict;
+
+###########################################################################################
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# #                                                                                       #
+# #             D I L B E R T   code                                                      #
+# #                                                                                       #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+###########################################################################################
+
+my $dilbert_mainpage = get('http://www.dilbert.com/fast/');
+my $dilbert_img_0000;
+($dilbert_img_0000) = ($dilbert_mainpage =~ m:(0000.*\.gif):);
+
+my $dilbert_magic_1 =  img({-src=>'http://dilbert.com/dyn/str_strip/'. $dilbert_img_0000 ,-align=>"right"});
+my $dilbert_magic = "<a href = http://www.dilbert.com/>" . $dilbert_magic_1 . "</a>";
+
+# We have now constructed the link we are looking for.  
+# dilbert_magic looks like: "<a href = http:...> http://www... </a>"
+
+print header(-expire => "-1d");
+
+open(HOME,"< ./index.html") || die "didn't open";
+
+while(<HOME>)
+{
+    if(m/statlist/)
+    {
+	print $dilbert_magic 
+    }
+    else 
+    {
+	if(m/randomImg/)
+	{
+	    if(rand() < .2)
+	    {   # lemmon hill
+		print '<a href="photos/lemmon_hill"> <img src="photos/lemmon_hill/lemmon_hill_suffering_1.jpg" border=0, Align="left"></A>';
+	    }
+	    else
+	    {
+		if(rand() < .1)
+		{  # weather
+		    print '<a href="http://forecast.weather.gov/MapClick.php?w0=t&w2=wc&w3=sfcwind&w4=sky&w5=pop&w6=rh&w8=rain&w9=snow&w10=fzg&w11=sleet&AheadHour=47&Submit=Submit&FcstType=graphical&textField1=39.95250&textField2=-75.16570&site=all"><img src="http://images.intellicast.com/WeatherImg/RadarLoop/bgm_None_anim.gif" border=0 Align="left"> </a>';
+		}
+		else
+		{   # SP over 200 years
+		    print '<a href="http://www.dailykos.com/story/2008/12/2/102214/940/743/668445"><IMG SRC="SP_from_1825.jpg" Align="left" border=0 alt="SP from 1825"></a>';
+		}
+	    }
+	}
+	else
+	{ # default
+	    print if !m/HTTP-EQUIV/;
+	}
+    }
+}
+   end_html;
+
+
+#   <IMG SRC="http://maps.weather.com/images/radar/single_site/phlloc_450x284.jpg" VSPACE=2 BORDER=0 ALT="Philadelphia, PA (19104) local radar">
+#    img({-src=>'http://128.255.244.60/graphs/' . 'pres00_WTA.gif'}),
+#    img({-src=>'http://maps.weather.com/images/maps/aviation/av10_720x486.jpg'}),
+
+
+
+
 
 # Read the main page of dilbert--find the link to the image and assign it to dilbert_img
 # http://umweb2.unitedmedia.com/comics/dilbert/index.html
@@ -29,33 +89,4 @@ use strict;
 # my $myyear = ($year+1900);
 # my $mydate =  "$myyear-$mon-$mday";
 # my $dilbert_img = "http://dilbert.com/dyn/str_strip/$dilbert_img_0000";
-
-
-my $dilbert_mainpage = get('http://www.dilbert.com/fast/');
-my $dilbert_img_0000;
-($dilbert_img_0000) = ($dilbert_mainpage =~ m:(0000.*\.gif):);
-
-
-my $dilbert_magic_1 =  img({-src=>'http://dilbert.com/dyn/str_strip/'. 
-                           $dilbert_img_0000 ,-align=>"right"});
-my $dilbert_magic = "<a href = http://www.dilbert.com/>" . $dilbert_magic_1 . "</a>";
-
-
-# dilbert_magic looks like: "<a href = http:...> http://www... </a>"
-
-print header(-expire => "-1d");
-
-open(HOME,"< ./index.html") || die "didn't open";
-
-while(<HOME>)
-{
-    print if !m/statlist/ && !m/HTTP-EQUIV/;
-    print $dilbert_magic if m/statlist/;
-}
-   end_html;
-
-
-#   <IMG SRC="http://maps.weather.com/images/radar/single_site/phlloc_450x284.jpg" VSPACE=2 BORDER=0 ALT="Philadelphia, PA (19104) local radar">
-#    img({-src=>'http://128.255.244.60/graphs/' . 'pres00_WTA.gif'}),
-#    img({-src=>'http://maps.weather.com/images/maps/aviation/av10_720x486.jpg'}),
 
