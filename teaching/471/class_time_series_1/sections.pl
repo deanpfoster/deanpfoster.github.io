ee0f70924a8618c19ffffb6803a4da8147152ed9
Dean Foster <dean@foster.net>
2012-02-06 19:55:26 -0500

    removed 471.

--- teaching/471/class_time_series_1/sections.pl
+++
@@ -1,9 +0,0 @@
-# LaTeX2HTML 96.1 (Feb 5, 1996)
-# Associate sections original text with physical files.
-
-$key = q/0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0/;
-$section_info{$key} = '0%:%class21.html%:%No Title' unless ($section_info{$key}); 
-$done{"class21.html"} = 1;
-
-1;
-
