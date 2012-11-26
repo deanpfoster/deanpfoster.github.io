4324e947bee243fbc2f123f2a48fd14e4a4f3473
Dean Foster <dean@foster.net>
2012-11-26 13:36:49 -0500

    updating to gosset.

--- index.pl
+++ index.pl
@@ -89,10 +89,10 @@ while(<HOME>)
 	    };
 	    if(!$done && ($r < .88))
 	    {   # patagonia
-#		<iframe
-#		    src="http://www.google.com/latitude/apps/badge/api?user=-1137818149277703619&type=iframe&maptype=roadmap"
-#		    width="850" height="1400" frameborder="0"></iframe>
-#		$done = 1;
+		<iframe
+		    src="http://www.google.com/latitude/apps/badge/api?user=-1137818149277703619&type=iframe&maptype=roadmap"
+		    width="850" height="1400" frameborder="0"></iframe>
+		$done = 1;
 	    };
 	    if(!$done && ($r < .92))
 	    {  # weather
